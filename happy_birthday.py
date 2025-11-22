4cee3509b521d4079518a7e97ef2a479022993d2
Vinit Limkar <149281650+Vvl1232@users.noreply.github.com>
2025-11-22 23:58:40 +0530

    Update happy_birthday.py

--- happy_birthday.py
+++ happy_birthday.py
@@ -32,7 +32,6 @@ st.snow()
 
 # Add an image for extra energy (replace with a valid image URL)
 st.markdown(
-   st.markdown(
     st.markdown("""
 <div style='text-align:center;'>
     <p style='font-size:1.2em; color:#FF1493;'>
@@ -49,3 +48,4 @@ st.markdown(
 </div>
 """, unsafe_allow_html=True)
 )
+
